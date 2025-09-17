43f0f57d912756176795a4370b6c10ee320e0eb9
Sergey Kuleshov <svyatogor@gmail.com>
2025-09-17 18:54:12 +0300

    Reduce terminal window opacity to 80%

--- packages/mac/wezterm/dot-config/wezterm/wezterm.lua
+++ packages/mac/wezterm/dot-config/wezterm/wezterm.lua
@@ -220,7 +220,7 @@ config.tab_max_width = 32
 config.hide_tab_bar_if_only_one_tab = true
 
 config.window_decorations = "RESIZE|MACOS_FORCE_ENABLE_SHADOW|MACOS_FORCE_SQUARE_CORNERS"
-config.window_background_opacity = 0.93
+config.window_background_opacity = 0.8
 config.macos_window_background_blur = 60
 
 config.use_cap_height_to_scale_fallback_fonts = true
