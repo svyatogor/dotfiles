53502c452d0fc8438425a05d122431999d3aa35b
Sergey Kuleshov <svyatogor@gmail.com>
2025-09-24 15:14:38 +0300

    feat(catppuccin-frappe): enable transparent nvim background

--- themes/catppuccin-frappe/nvim.lua
+++ themes/catppuccin-frappe/nvim.lua
@@ -1,9 +1,11 @@
 return {
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "catppuccin-frappe",
-    },
-  },
+	{ "catppuccin/nvim", name = "catppuccin", priority = 1000, opts = {
+		transparent_background = true,
+	} },
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "catppuccin-frappe",
+		},
+	},
 }
