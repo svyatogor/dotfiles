97197a4a585499aec38ba8d586a5eb9d29e9ff2e
Sergey Kuleshov <svyatogor@gmail.com>
2026-02-24 17:02:24 +0200

    chore: update dotfiles configurations and dependencies
    - integrate Neogit plugin for enhanced Git workflow in Neovim
    - bump various Neovim plugin versions
    - adjust Aerospace outer gaps and add smart resize keybindings
    - refine Aerospace service mode keybindings and disable default floating layout

--- packages/common/nvim/dot-config/nvim/lua/plugins/git.lua
+++ packages/common/nvim/dot-config/nvim/lua/plugins/git.lua
@@ -7,4 +7,25 @@ return {
       { "<leader>gD", "<cmd>DiffviewOpen<CR>", desc = "Open Diff View" },
     },
   },
+  {
+    "NeogitOrg/neogit",
+    lazy = true,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "sindrets/diffview.nvim",
+      "nvim-telescope/telescope.nvim",
+    },
+    cmd = "Neogit",
+    keys = {
+      { "<leader>gg", "<cmd>Neogit<cr>", desc = "Show Neogit UI" },
+    },
+  },
+  -- Disable lazygit bindings from snacks
+  {
+    "folke/snacks.nvim",
+    keys = {
+      { "<leader>gg", false },
+      { "<leader>gG", false },
+    },
+  },
 }
