f68d2cfa30fa21d8bcc78efb791c42cfa8921d4c
Sergey Kuleshov <svyatogor@gmail.com>
2026-03-19 21:06:09 +0200

    chore(fish): remove fish shell config package
    - delete all tracked fish config files: config.fish, fish_plugins, conf.d, functions, and completions
    - touches(starship): add hostname module with SSH symbol formatting

--- packages/common/fish/dot-config/fish/completions/gwd.fish
+++
@@ -1,7 +0,0 @@
-complete -c gwd -f -a '(
-    set -l wt_root (path dirname (git rev-parse --absolute-git-dir) 2>/dev/null)
-    git worktree list --porcelain 2>/dev/null | string replace -rf "^worktree (.+)" "\$1" | while read -l wt
-        test "$wt" = "$wt_root"; and continue
-        path basename $wt
-    end
-)'
--- packages/common/fish/dot-config/fish/completions/gwt.fish
+++
@@ -1,1 +0,0 @@
-complete -c gwt -f -a '(git branch -a --format="%(refname:short)" 2>/dev/null | string replace -r "^origin/" "" | sort -u)'
