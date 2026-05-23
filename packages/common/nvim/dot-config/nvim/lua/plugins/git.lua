48ba0b24925917b39f0dd79b842d68e1c87c865c
Sergey Kuleshov <svyatogor@gmail.com>
2026-05-23 12:21:08 +0300

    chore(nvim): update to supported fork of diffview

--- packages/common/nvim/dot-config/nvim/lua/plugins/git.lua
+++ packages/common/nvim/dot-config/nvim/lua/plugins/git.lua
@@ -1,10 +1,22 @@
 return {
   { "tpope/vim-fugitive" },
   {
-    "sindrets/diffview.nvim",
+    "dlyongemallo/diffview.nvim",
+    version = "v0.32",
     cmd = "DiffviewOpen",
     keys = {
       { "<leader>gD", "<cmd>DiffviewOpen<CR>", desc = "Open Diff View" },
+    },
+    opts = {
+      enhanced_diff_hl = true,
+      view = {
+        cycle_layouts = {
+          default = { "diff2_horizontal", "diff1_inline" },
+        },
+        inline = { style = "overleaf" },
+        default = { layout = "diff1_inline" },
+        merge_tool = { layout = "diff3_horizontal" },
+      },
     },
   },
   {
@@ -12,12 +24,15 @@ return {
     lazy = true,
     dependencies = {
       "nvim-lua/plenary.nvim",
-      "sindrets/diffview.nvim",
+      "dlyongemallo/diffview.nvim",
       "nvim-telescope/telescope.nvim",
     },
     cmd = "Neogit",
     keys = {
       { "<leader>gg", "<cmd>Neogit<cr>", desc = "Show Neogit UI" },
+    },
+    opts = {
+      integrations = { diffview = true },
     },
   },
   -- Disable lazygit bindings from snacks
